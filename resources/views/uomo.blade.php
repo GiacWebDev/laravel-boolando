fcfc4770d29d55b18a02feac5ea43313b7c362da
GiacWebDev <giacobechini@gmail.com>
2023-11-18 16:22:06 +0100

    [add] _universal_setting (as main in vue) [add] _main.scss [add] main.php

--- resources/views/uomo.blade.php
+++ resources/views/uomo.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Uomo</h1>
+    <div class="container">
+
+        <h1>Uomo</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore provident quos ex, aliquid repellendus, repellat
+            quis, nihil magnam odio nam omnis nostrum sint iste dolorem explicabo atque. Aspernatur, voluptatibus rerum.
+        </p>
+
+    </div>
 @endsection
