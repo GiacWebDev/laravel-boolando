1ff4aaa5576c93744cb6397352b65c4852009d2d
GiacWebDev <giacobechini@gmail.com>
2023-11-19 16:22:58 +0100

    [edit] dynamic card

--- resources/views/uomo.blade.php
+++ resources/views/uomo.blade.php
@@ -2,37 +2,38 @@
 
 @section('content')
     <div class="container">
+        @forelse ($products as $product)
+            <div class="card">
+                <div class="top">
+                    <img src="{{ $product['primaryImage'] }}" alt="{{ $product['modello'] }}" class="image-prod">
+                    <img src="{{ $product['secondaryImage'] }}" alt="{{ $product['modello'] }}" class="img-hover">
 
-
-        <div class="card">
-            <div class="top">
-                <img src="/img/products/1.webp" alt="modello" class="image-prod">
-                <img src="/img/products/1b.webp" alt="modello" class="img-hover">
-
-                <div class="heart">
-                    <span class="icon">&hearts;</span>
-                </div>
-                <div class="banner">
-                    <div class="red-discount">
-                        <p>-50%</p>
+                    <div class="heart">
+                        <span class="icon">&hearts;</span>
                     </div>
-                    <div class="green">
-                        {{-- <p>{{ productObj . sostenibilita }}</p>
+                    <div class="banner">
+                        <div class="red-discount">
+                            <p>{{ $product['discount'] }}</p>
+                        </div>
+                        <div class="green">
+                            {{-- <p>{{ productObj . sostenibilita }}</p>
                     </div> --}}
+                        </div>
+                    </div>
+                    <div class="bottom">
+                        <p>{{ $product['marca'] }}</p>
+                        <h2>{{ $product['modello'] }}</h2>
+                        <p><span class="price">{{ $product['fullPrice'] }}</span>
+                            <span class="discount">{{ $product['lastPrice'] }}</span>
+                        </p>
                     </div>
                 </div>
-                <div class="bottom">
-                    <p>Levi's</p>
-                    <h2>RELAXED FIT TEE UNISEX</h2>
-                    <p><span class="price">29.99</span>
-                        <span class="discount">20.99</span>
-                    </p>
-                </div>
-            </div>
+            @empty
+                <h2>Non ci sono prodotti</h2>
+        @endforelse
+    </div>
+@endsection
 
-        </div>
-    @endsection
-
-    @section('title')
-        | Uomo
-    @endsection
+@section('title')
+    | Uomo
+@endsection
